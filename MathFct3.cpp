46c579fe3e0d4a8ef49e65816cf594fd2792d0a0
Bill Bai <billbai0102@gmail.com>
2019-11-11 09:30:22 -0500

    completed mathfct3.c

--- MathFct3.cpp
+++ MathFct3.cpp
@@ -2,11 +2,13 @@
     Name: Bill
     Teacher: Mr. Wong
     Date: 2019-11-08
-    This program =======================
+    This program demonstrates two new Math functions that are very useful.
 */
 
 #include <stdio.h>
 #include <math.h> // Header files needed for Math functions
+
+double num; // Global variable
 
 //Function title - Prints the title of program
 void title(){
@@ -14,6 +16,40 @@ void title(){
 }
 
 // Function spacing - create a number of blank lines, and then a number of spaces based on calling arguments
-void spacing(){
+void spacing(int numOfLines, int numOfSpaces){
+    for(int x = 0; x < numOfLines; x++)
+        printf("\n");
+    for(int x = 0; x < numOfSpaces; x++)
+        printf(" ");
+}
 
+//Function intro - prints program's introduction.
+void intro(){
+    title();
+    spacing(0, 5);
+    printf("This program demonstrates two new Math functions that are very useful!");
+}
+
+void userInput(){
+    spacing(2,0);
+    printf("Enter a real number: ");
+    scanf("%lf", &num);
+    getchar();
+}
+
+//Function display - Displays program excluding intro.
+void display(){
+    double answer;
+    spacing(3, 30);
+    answer = round(num);
+    printf("%lf is rounded as %lf\n", num, answer);
+    spacing(1, 30);
+    printf("%lf to the power of 3 is %lf\n", num, pow(num, 3));
+}
+
+main(){
+    intro();
+    userInput();
+    display();
+    getchar();
 }
