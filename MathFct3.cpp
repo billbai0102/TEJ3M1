a1f21cdf862dc557de047510c00a86e79161428e
Bill Bai <billbai0102@gmail.com>
2019-11-11 09:20:30 -0500

    Update MathFct3.cpp

--- MathFct3.cpp
+++ MathFct3.cpp
@@ -2,8 +2,18 @@
     Name: Bill
     Teacher: Mr. Wong
     Date: 2019-11-08
-    This program outputs centerd text.
+    This program =======================
 */
 
 #include <stdio.h>
-#include <math.h>
+#include <math.h> // Header files needed for Math functions
+
+//Function title - Prints the title of program
+void title(){
+    printf("\n%30sMore Math Functions\n", "");
+}
+
+// Function spacing - create a number of blank lines, and then a number of spaces based on calling arguments
+void spacing(){
+
+}
