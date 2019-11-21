26e92a30cdb028b362844a3ab9167b358a46673c
Bill Bai <31229051+billbai0102@users.noreply.github.com>
2019-11-21 09:53:27 -0500

    Update Ex8.cpp

--- Ex8.cpp
+++ Ex8.cpp
@@ -1,18 +1,29 @@
+/*
+    Name: Bill
+    Teacher: Mr. Wong
+    Date: 2019-11-22
+    This program vertically and horizonally centers a string.
+*/
+
 #include <stdio.h>
 #include <string.h>
 
+// String that will be centered
+char text [] = "12345678901234567890";
 
-char people [] = "HELLO";
-
+// Displays the vertically and horizontally centered text.
 void display(){
-    int verticalCenter = 30  - (strlen(people)/2);
+    int verticalCenter = 43 * 0.5  - (strlen(text)*0.5); // Vertical center index
+    // Adds blank lines to vertically center text
     for(int i = 0; i < verticalCenter; i++)
         printf("\n");
-    for(int i = 0; i < strlen(people); i++){
-        printf("%39c\n", people[i]);
+    // Prints the text vertically, and horizontally centered
+    for(int i = 0; i < strlen(text); i++){
+        printf("%40c\n", text[i]);
     }
 }
 
+// Main program - controls program flow.
 int main(){
     display();
     getchar();
